f4d205b554339752045bb7934faa17e93dec65af
AditiShrivastava28 <aditishrivastava2822003@gmail.com>
2026-01-05 14:22:17 +0530

    error redolev

--- index.tsx
+++ index.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Fix: Use interface augmentation to add API_KEY to the environment variables.
+// This avoids the "Subsequent variable declarations must have the same type" error
+// that occurs when redeclaring the global 'process' variable.
+declare global {
+  interface ProcessEnv {
+    API_KEY: string;
+    [key: string]: string | undefined;
+  }
+
+  interface Process {
+    env: ProcessEnv;
+  }
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
